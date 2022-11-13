cd564ca7a879993329846641bac8b6c9471a5965
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-11-13 15:01:58 -0300

    exemplo lista dinâmica

--- src/components/lists/index.tsx
+++ src/components/lists/index.tsx
@@ -3,28 +3,35 @@ import React from "react";
 // Function Components -> Forma moderna de escrever componentes em React. 
 
 const List = (): JSX.Element => {
+    const tasks = [{
+        task: "React",
+        time: "02:00"
+    }, {
+        task: "Javascript",
+        time: "01:00"
+    }, {
+        task: "Typescript",
+        time: "03:00"
+    }];
+
     return (
         <aside>
             <h2>
                 Estudos do dia
             </h2>
             <ul>
-                <li>
-                    <h3>
-                        React
-                    </h3>
-                    <span>
-                        02:00
-                    </span>
-                </li>
-                <li>
-                    <h3>
-                        Javascript
-                    </h3>
-                    <span>
-                        01:00
-                    </span>
-                </li>
+                {
+                    tasks.map(({ task, time }, index) => (
+                        <li key={index}>
+                            <h3>
+                                {task}
+                            </h3>
+                            <span>
+                                {time}
+                            </span>
+                        </li>
+                    ))
+                }
             </ul>
         </aside>
     )
