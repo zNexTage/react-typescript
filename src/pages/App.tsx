80444d98095349c55005762ebd12c2296a12af57
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-03 12:47:06 -0300

    Add Stopwatch to App.

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from '../components/forms';
 import List from '../components/lists';
+import Stopwatch from '../components/stopwatch/stopwatch';
 import style from './styles.module.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className={style.AppStyle}>
       <Form />
       <List />
+      <Stopwatch />
     </div>
   );
 }
