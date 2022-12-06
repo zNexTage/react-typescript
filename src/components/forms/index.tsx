ff22efdbd7d31bfc797e0cd84b17132417cbf907
Christian <christianoliveira1908@hotmail.com>
2022-12-06 09:34:39 -0300

    use of state and submit event

--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import Button from '../button';
 import style from './styles.module.scss';
+
 class Form extends React.Component {
+    state = {
+        task: "",
+        time: "00:00"
+    }
+
+    //Set state after user input in a field.
+    private handleChange(key: string, value: string) {
+        this.setState({
+            ...this.state,
+            [key]: value
+        });
+    }
+
+    /**
+     * Add a task
+     * @param event 
+     */
+    private onSubmit(event: React.FormEvent<HTMLFormElement>) {
+        // avoids the default behavior of submit, in this case, it prevents the page from being updated.
+        event.preventDefault();
+
+
+    }
+
     render(): React.ReactNode {
         return (
-            <form className={style.novaTarefa}>
+            <form className={style.novaTarefa} onSubmit={this.onSubmit.bind(this)}> {/* this.onSubmit.bind(this) -> binds submit to the scope of the class */}
                 <div className={style.inputContainer}>
                     <label htmlFor='txtTask'>
                         Adicione um novo estudo
@@ -14,6 +39,8 @@ class Form extends React.Component {
                         placeholder="O que você quer estudar?"
                         type="text"
                         name="task"
+                        onChange={event => this.handleChange('task', event.target.value)}
+                        value={this.state.task}
                         required
                     />
                 </div>
@@ -21,6 +48,7 @@ class Form extends React.Component {
                     <label htmlFor='txtTime'>
                         Tempo
                     </label>
+                    {/* Controlled input */}
                     <input
                         id="txtTime"
                         step={1}
@@ -29,6 +57,8 @@ class Form extends React.Component {
                         placeholder="O que você quer estudar?"
                         type="time"
                         name="time"
+                        value={this.state.time}
+                        onChange={event => this.handleChange('time', event.target.value)}
                         required
                     />
                 </div>
