cc7d21c7e5b2a0592f58b4b6dba0a31cd616adb0
Christian de Oliveira <christianoliveira1908@hotmail.com>
2022-12-07 08:37:38 -0300

    click event to select item in list

--- src/components/lists/index.tsx
+++ src/components/lists/index.tsx
@@ -3,12 +3,34 @@ import ITask from "../../types/task";
 import style from "./style.module.scss";
 
 
-interface IProps {
-    tasks: Array<ITask>
+interface IListProps {
+    tasks: Array<ITask>;
+    onTaskClick: (task: ITask) => void;
 }
 
-// Function Components -> Forma moderna de escrever componentes em React. 
-const List = ({ tasks }: IProps): JSX.Element => {
+interface IItemProps {
+    task: ITask;
+    onTaskClick: (task: ITask) => void;
+}
+
+const Item = ({ onTaskClick, task }: IItemProps) => {
+    return (
+        <li
+            onClick={() => onTaskClick(task)}
+            key={task.id}
+            className={style.item}>
+            <h3>
+                {task.task}
+            </h3>
+            <span>
+                {task.time}
+            </span>
+        </li>
+    )
+}
+
+// Function Components -> Modern way to write components
+const List = ({ tasks, onTaskClick }: IListProps): JSX.Element => {
 
     return (
         <aside className={style.listaTarefas}>
@@ -17,17 +39,12 @@ const List = ({ tasks }: IProps): JSX.Element => {
             </h2>
             <ul>
                 {
-                    tasks.map(({ task, time, id, completed, selected }, index) => (
-                        <li
-                            key={id}
-                            className={style.item}>
-                            <h3>
-                                {task}
-                            </h3>
-                            <span>
-                                {time}
-                            </span>
-                        </li>
+                    tasks.map((task, index) => (
+                        <Item
+                            key={task.id}
+                            task={task}
+                            onTaskClick={onTaskClick}
+                        />
                     ))
                 }
             </ul>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,13 +7,21 @@ import style from './styles.module.scss';
 
 function App() {
   const [tasks, setTasks] = useState(new Array<ITask>());
+  const [selectedTask, setSelectedTask] = useState<ITask>();
 
   const onSubmit = (task: ITask) => setTasks([...tasks, task]);
+
+  const onTaskClick = (task: ITask) => {
+    setSelectedTask(task);
+  }
 
   return (
     <div className={style.AppStyle}>
       <Form onSubmit={onSubmit} />
-      <List tasks={tasks} />
+      <List
+        tasks={tasks}
+        onTaskClick={onTaskClick}
+      />
       <Stopwatch />
     </div>
   );
